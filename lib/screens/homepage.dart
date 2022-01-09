9ae2e25b95aaf97725ae879afb828fc966ff4261
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-01-09 15:32:16 -0500

    Commit after the restructure the app.

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tiktok_clone/main.dart';
+import 'package:tiktok_clone/widgets/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
